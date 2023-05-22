4153918dda052cc3124b9a4cd2f862be6eb93548
Jerson Misas <camilomisas0@gmail.com>
2023-05-22 17:34:33 -0500

    Se Terminó el menú responsive. Se argegó un nuevo icono para cerrar el menú

--- Js/index.js
+++ Js/index.js
@@ -1,11 +1,18 @@
 
 var iconoUno = document.getElementById("iconoUno");
+var iconoDos = document.getElementById("iconoDos");
 var lista = document.getElementById("lista");
+
+iconoDos.style.display = "none";
 function Lista(){
     lista.style.display = "block";
     lista.style.width = "300px"
     iconoUno.style.cursor = "pointer"
+    iconoDos.style.display = "block";
+    iconoUno.style.display = "none";
 }
 function ocultarLista(){
     lista.style.display = "none";
+    iconoDos.style.display = "none";
+    iconoUno.style.display = "block";
 }
