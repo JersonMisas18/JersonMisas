288213b0f594a6942ea05d3861240474d5405ee1
Jerson Misas <camilomisas0@gmail.com>
2023-05-22 16:36:10 -0500

    Se Terminó el menú en la versión responsive

---
+++ Js/index.js
@@ -0,0 +1,11 @@
+
+var iconoUno = document.getElementById("iconoUno");
+var lista = document.getElementById("lista");
+function Lista(){
+    lista.style.display = "block";
+    lista.style.width = "300px"
+    iconoUno.style.cursor = "pointer"
+}
+function ocultarLista(){
+    lista.style.display = "none";
+}
